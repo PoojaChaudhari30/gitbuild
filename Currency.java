2f42e5696f8dcda50084bbaa820889b3c010d391
PoojaChaudhari30 <poojac9318@gmail.com>
2025-05-23 17:59:30 +0530

    Add files via upload
    commit

---
+++ Currency.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Currency{
+	
+	public static void main(String[] args)
+	
+	{
+		ArrayList<String> arr = new ArrayList<String> ();
+		arr.add("POUNDS");
+		arr.add("DOLLARS");
+		arr.add("EUROS");
+		
+		for(int i=0;i<arr.size();i++) 
+		{
+			if(arr.contains("POUNDS"))
+			{
+				arr.get(i);
+				System.out.println(arr);
+			}
+			else
+			{
+				System.out.println("POUNDS NOT FOUND");
+			}
+		}
+	}
+}
