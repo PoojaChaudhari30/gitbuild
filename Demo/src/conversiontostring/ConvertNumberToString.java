879652d7b32c209c4a3dd8db53d7aae11fc170b6
Win <Win@Win-PC>
2025-05-24 16:05:46 +0530

    code commit

---
+++ Demo/src/conversiontostring/ConvertNumberToString.java
@@ -0,0 +1,12 @@
+package conversiontostring;
+
+public class ConvertNumberToString {
+
+	public static void main(String[] args) {
+		int num = 123456;
+		String str = String.valueOf(num);
+		System.out.println(num);
+		
+	}
+
+}
